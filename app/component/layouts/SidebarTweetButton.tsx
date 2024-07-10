4bc4e73c69e78d9f0c1e1e4d7a8496caea1bfb07
om salunke <salunkeom474@gmail.com>
2024-07-09 20:45:40 -0700

    model created for user post notification and comment

--- app/component/layouts/SidebarTweetButton.tsx
+++ app/component/layouts/SidebarTweetButton.tsx
@@ -1,18 +1,25 @@
+"use client"
 import { useRouter } from 'next/navigation'
-import React from 'react'
+import React, { useCallback } from 'react'
 import { FaFeather } from 'react-icons/fa'
+import useLoginModal from '../hooks/UserLoginModal'
 
 function SidebarTweetButton() {
     const router=useRouter()
+    const loginModal=useLoginModal()
+    const onclick=useCallback(()=>{
+loginModal.onOpen()
+    },[loginModal])
   return (
     <>
     <div
     className='mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex items-center justify-center bg-sky-500 hover:bg-opacity-80 transition cursor-pointer'
-    onClick={()=>router.push("/")}
+    onClick={onclick}
     >
      <FaFeather size={24} color='white'/>
         </div>
-        <div className='mt-6 hidden lg:block px-4 py-2 rounded-full bg-sky-500 hover:bg-opacity-90 cursor-pointer transistion'>
+        <div     onClick={onclick}
+ className='mt-6 hidden lg:block px-4 py-2 rounded-full bg-sky-500 hover:bg-opacity-90 cursor-pointer transistion'>
 <p className='hidden lg:block text-center font-semibold text-white text-[20px]'>Tweet</p>
         </div>
         </>
